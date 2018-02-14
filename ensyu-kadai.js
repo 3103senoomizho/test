1a990ed21b61e9b6b540846a05c0b8b7408f5d97
3103senoomizho <pjwz1437@yahoo.co.jp>
2018-02-14 17:58:30 +0900

    kome

---
+++ ensyu-kadai.js
@@ -0,0 +1,27 @@
+//問１　半径5cm、7cm、10cmの面積
+function Menseki(hankei,ritsu=3.14){
+    return hankei*hankei*ritsu;
+}
+
+document.write(Menseki(5));
+document.write("<br>");
+document.write(Menseki(7));
+document.write("<br>");
+document.write(Menseki(10));
+document.write("<br>");
+
+document.write("<br>")
+
+//問２　
+
+function totalRyokin(oNi,kNi,oRyo=500,kRyo=200){
+    return (oRyo*oNi)+(kNi*kRyo);
+}
+
+document.write(totalRyokin(2,4));
+document.write("円です。");
+document.write("<br>");
+document.write(totalRyokin(1,5));
+document.write("<br>");
+document.write(totalRyokin(3,7));
+document.write("<br>");
